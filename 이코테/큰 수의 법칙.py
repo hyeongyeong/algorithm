a7abb21c22fe3b25c0eee59fa799bc20626383b2
hyeon <33833731+hyeongyeong@users.noreply.github.com>
2021-07-15 13:13:18 +0900

    Update 큰 수의 법칙.py

--- 이코테/큰 수의 법칙.py
+++ 이코테/큰 수의 법칙.py
@@ -1,3 +1,8 @@
+# 문제 :
+# n 크기의 array
+# array 안의 숫자에서 숫자들을 총 m번 사용하여 최대합 구하기
+# 단, array 안의 숫자들은 중복 가능하며 최대 k번 중복 허용
+
 n, m, k = map(int, input().split())
 data = list(map(int, input().split()))
 
